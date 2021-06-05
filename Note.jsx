d15763888ff0e834ebb1195008a6cc5d9b3fd150
Ankitwadhwa1402 <67230452+Ankitwadhwa1402@users.noreply.github.com>
2021-06-05 18:48:52 +0530

    Add files via upload

---
+++ Note.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+function Note() {
+  return (
+    <div className="note">
+      <h1>JaVascript and React.js</h1>
+      <p>
+        This was the 7 day botcamp. we have covered HTML,JaVascript and React.js
+      </p>
+    </div>
+  );
+}
+export default Note;
---
+++ index.js
@@ -0,0 +1,10 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./components/apps";
+
+ReactDOM.render(
+  <div>
+    <App />
+  </div>,
+  document.getElementById("root")
+);
